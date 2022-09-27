2b203440841f05c4f47f77508fc0e3b1389091d5
Alec Bongard <alec.bongard@gmail.com>
2022-09-27 14:09:25 -0700

    Complete third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    newstring = '';
+    strArray = string.split('');
+    while (strArray.length){
+        newstring = newstring + strArray.pop();
+    }
+    return newstring
 };
 
 // Do not edit below this line
