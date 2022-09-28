375ad4707cdaa8dd1e2163aa3e422cb75f9c0995
Alec Bongard <alec.bongard@gmail.com>
2022-09-28 11:22:45 -0700

    Complete fourth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ) {
+    let removals = Array.from(arguments);
+    removals.splice(0,1);
+    for (elem of removals){
+        for(let i = 0;i<arr.length;i++){
+            if (arr[i] === elem){
+                arr.splice(i, 1);
+            }
+        }
+    }
+    return arr;
 
 };
 
