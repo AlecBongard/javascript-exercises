4db5bacbe3f752b5e1cb516d6c626d8495c5a8da
Alec Bongard <alec.bongard@gmail.com>
2022-09-28 11:32:51 -0700

    Complete fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    if(first<0 || second<0 ||
+         typeof(first) !== "number" || typeof(second)!== "number"){
+        return 'ERROR';
+    }
 
+
+
+    let low = Math.min(first, second);
+    let high = Math.max(first, second);
+    let sum = 0;
+    for(let i = low; i<=high;i++){
+        sum+=i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
