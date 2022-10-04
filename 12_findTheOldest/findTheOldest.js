0fbd96e2ac611f17a40858f4d31d955677170e15
Alec Bongard <alec.bongard@gmail.com>
2022-10-04 13:53:00 -0700

    Complete twelfthexercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const now = new Date();
+    const currentYear = now.getFullYear();
+    
+    let oldest = arr[0];
+    let oldAge = 0;
 
+    for(person of arr){
+        let pAge=0;
+        if('yearOfDeath' in person){
+            pAge = person.yearOfDeath - person.yearOfBirth;
+        } else{
+            pAge = currentYear - person.yearOfBirth;
+        }
+
+        if(pAge > oldAge){
+            oldest = person;
+            oldAge = pAge;
+        }
+
+    }
+
+    return oldest;
 };
 
 // Do not edit below this line
