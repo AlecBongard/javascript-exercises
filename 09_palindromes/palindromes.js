8b870440f0f1fda071830562179e7be225a2cc4d
Alec Bongard <alec.bongard@gmail.com>
2022-10-04 12:46:03 -0700

    Complete ninth exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.toLowerCase().replace(/[^a-z]/g, '');
+    let reversed = str.slice(-1);
 
+    for(let i = 2; i <= str.length;i++){
+        reversed = reversed + str.slice(-i, -(i - 1))
+    }
+
+    if(reversed === str){
+        return true;
+    }else{
+        return false;
+    }
 };
 
 // Do not edit below this line
