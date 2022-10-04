f5d3571c89cd241a18456e509099458c3bcf3314
Alec Bongard <alec.bongard@gmail.com>
2022-10-04 13:07:41 -0700

    Complete tenth exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(place) {
+    if(place < 1){
+        return "OOPS";
+    }else if(typeof place === "string"){
+        place = parseInt(place);
+    }
 
+    if(place === 1 || place === 2){
+        return 1;
+    }else{
+        return fibonacci(place - 1) + fibonacci(place - 2);
+    }
 };
 
 // Do not edit below this line
